1053d7f9e66b0e025736fcb285089a9d9fb585ae
Tom Wilson <tom@jackhq.com>
2011-04-15 23:39:56 -0400

    update redis connection string

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class Cloudq < Sinatra::Base
     params['job'] = JSON.parse(data)['job']
     
     # push into redis
-    redis = EM::Protocols::Redis.connect(ENV['REDISTOGO_URL'])
+    redis = EM::Protocols::Redis.connect(:host => ENV['REDISTOGO_URL'])
     redis.push_head [q,'-',:queued].join(''), params['job'].to_json
 
     body "Success"
@@ -29,7 +29,7 @@ class Cloudq < Sinatra::Base
 
   # Get Job from the Queue
   aget "/:queue" do |q|
-    redis = EM::Protocols::Redis.connect(ENV['REDISTOGO_URL'])
+    redis = EM::Protocols::Redis.connect(:host => ENV['REDISTOGO_URL'])
     redis.rpop [q,'-',:queued].join('') do |response|
       if response 
         id = UUIDTools::UUID.random_create.to_s 
@@ -45,7 +45,7 @@ class Cloudq < Sinatra::Base
   # Remove Job from the Queue
 
   adelete "/:queue/:id" do |q, id|
-    redis = EM::Protocols::Redis.connect(ENV['REDISTOGO_URL'])
+    redis = EM::Protocols::Redis.connect(:host => ENV['REDISTOGO_URL'])
     redis.delete id
     body "Success"
   end
